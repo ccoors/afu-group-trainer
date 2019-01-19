84065cabea4eb94353edfd4cba2610ec49d0d9f3
Christian Friedrich Coors <me@ccoors.de>
2019-01-19 20:43:06 +0100

    Improve favicon at lower resolutions

--- frontend/resources/genFavicon.sh
+++ frontend/resources/genFavicon.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
 set -e
-sizes=(16 24 32 48 64 128 144 180 192 256 512 1024)
+lowres_sizes=(16 24 32 48)
+sizes=(64 128 144 180 192 256 512 1024)
 input="favicon"
+
+for size in "${lowres_sizes[@]}"; do
+    inkscape -z -e ${input}_${size}.png -w $size -h $size ${input}_lowres.svg
+done
 
 for size in "${sizes[@]}"; do
     inkscape -z -e ${input}_${size}.png -w $size -h $size $input.svg
