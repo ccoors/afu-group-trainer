d9d3f8ae293de34ecc4270e9c15ec0410cc3bb01
Christian Friedrich Coors <me@ccoors.de>
2019-04-23 22:26:45 +0200

    Move official repository to GitHub

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -11,7 +11,7 @@ class AppFooter extends React.Component {
                     <List id="footerList" floated="right" horizontal size="small">
                         <List.Item>{this.props.version}</List.Item>
                         {this.props.footerLink}
-                        <List.Item href={"https://gitea.ccoors.de/ccoors/afu-group-trainer"} target={"_blank"}
+                        <List.Item href={"https://github.com/ccoors/afu-group-trainer"} target={"_blank"}
                                    rel={"noopener noreferrer"}>Source</List.Item>
                     </List>
                 </div>
